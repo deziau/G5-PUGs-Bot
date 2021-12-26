1b33f192c76b3b2580db098429de729993631c7c
TheBO$$ <40491968+thboss@users.noreply.github.com>
2021-12-26 09:40:36 +0300

    refactor migrations

--- migrations/20211226_01_aVejE-create-base-tables.py
+++ migrations/20211226_01_aVejE-create-base-tables.py
@@ -1,8 +1,10 @@
+"""
+Create base tables
+"""
 
 from yoyo import step
 
-
-__depends__ = {'20210804_01_guilds-table'}
+__depends__ = {}
 
 steps = [
     step(
@@ -19,10 +21,24 @@ steps = [
     ),
     step(
         (
+            'CREATE TABLE guilds(\n'
+            '    id BIGSERIAL PRIMARY KEY,\n'
+            '    api_key VARCHAR(128) DEFAULT NULL,\n'
+            '    linked_role BIGINT DEFAULT NULL,\n'
+            '    prematch_channel BIGINT DEFAULT NULL,\n'
+            '    category BIGINT DEFAULT NULL,\n'
+            '    lobbies_channel BIGINT DEFAULT NULL\n'
+            ');'
+        ),
+        'DROP TABLE guilds;'
+    ),
+    step(
+        (
             'CREATE TABLE lobbies(\n'
             '    id SMALLSERIAL PRIMARY KEY,\n'
             '    guild BIGINT DEFAULT NULL REFERENCES guilds (id) ON DELETE CASCADE,\n'
             '    name VARCHAR(64) DEFAULT NULL,\n'
+            '    region VARCHAR(3) DEFAULT NULL,\n'
             '    capacity SMALLINT DEFAULT 10,\n'
             '    series_type series_type DEFAULT \'bo1\',\n'
             '    team_method team_method DEFAULT \'captains\',\n'
@@ -51,6 +67,31 @@ steps = [
     ),
     step(
         (
+            'CREATE TABLE matches(\n'
+            '    id SMALLSERIAL PRIMARY KEY,\n'
+            '    guild BIGINT DEFAULT NULL REFERENCES guilds (id) ON DELETE CASCADE,\n'
+            '    channel BIGINT DEFAULT NULL,\n'
+            '    message BIGINT DEFAULT NULL,\n'
+            '    category BIGINT DEFAULT NULL,\n'
+            '    team1_channel BIGINT DEFAULT NULL,\n'
+            '    team2_channel BIGINT DEFAULT NULL\n'
+            ');'
+        ),
+        'DROP TABLE matches;'
+    ),
+    step(
+        (
+            'CREATE TABLE users('
+            '    discord_id BIGSERIAL UNIQUE,\n'
+            '    steam_id VARCHAR(18) UNIQUE,\n'
+            '    flag VARCHAR(3) DEFAULT NULL,\n'
+            '    PRIMARY KEY (discord_id, steam_id)\n'
+            ');'
+        ),
+        'DROP TABLE users;'
+    ),
+    step(
+        (
             'CREATE TABLE queued_users(\n'
             '    lobby_id BIGSERIAL REFERENCES lobbies (id) ON DELETE CASCADE,\n'
             '    user_id BIGSERIAL REFERENCES users (discord_id) ON DELETE CASCADE,\n'
@@ -58,5 +99,26 @@ steps = [
             ');'
         ),
         'DROP TABLE queued_users;'
+    ),
+    step(
+        (
+            'CREATE TABLE match_users(\n'
+            '    match_id SMALLSERIAL REFERENCES matches (id) ON DELETE CASCADE,\n'
+            '    user_id BIGSERIAL REFERENCES users (discord_id),\n'
+            '    CONSTRAINT match_user_pkey PRIMARY KEY (match_id, user_id)\n'
+            ');'
+        ),
+        'DROP TABLE match_users;'
+    ),
+    step(
+        (
+            'CREATE TABLE banned_users(\n'
+            '    guild_id BIGSERIAL REFERENCES guilds (id) ON DELETE CASCADE,\n'
+            '    user_id BIGSERIAL REFERENCES users (discord_id),\n'
+            '    unban_time TIMESTAMP WITH TIME ZONE DEFAULT null,\n'
+            '    CONSTRAINT banned_user_pkey PRIMARY KEY (guild_id, user_id)\n'
+            ');'
+        ),
+        'DROP TABLE banned_users;'
     )
 ]
